5a74926774d42e58bca576bc4ae148437dbd32b6
LuisD <cusiluis04@gmail.com>
2026-06-02 17:13:17 -0500

    view category

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Context;
+using E_Commerce.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController(AppDbContext _dbContext) : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();    
+            return View(categories);
+        }
+    }
+}
